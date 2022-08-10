f85b2ec4a15071b6322f377536c73040ddd6c8fe
AdrianGrepe <62662654+AdrianGrepe@users.noreply.github.com>
2022-08-10 11:24:13 -0500

    Adding database

--- Growing_Enterprises/Enterprise/models.py
+++ Growing_Enterprises/Enterprise/models.py
@@ -1,3 +1,24 @@
+#Django
 from django.db import models
 
-# Create your models here.
+from User.models import User, UserProfile
+
+
+#Enterprise info
+class EnterpriseProfile(models.Model):
+   user = models.ForeignKey(User, on_delete=models.PROTECT, primary_key=True)
+   enterprise_register_date = models.DateField(default=None, auto_now_add=True)
+   business_name = models.CharField(max_length=250, default=None, null=False, blank=False)
+   tax_regime = models.CharField(max_length=250, default=None, null=False, blank=True)
+   enterprise_email = models.EmailField(unique=True, null=True, blank=True)
+   enterprise_phone_number = models.DecimalField(max_digits=16, unique=False, null=True, blank=True)
+   enterprise_date_of_birth = models.DateField(default=None, null=True)
+   enterprice_place_of_birth = models.CharField(max_length=64, blank=True)
+   enterprice_country = models.CharField(max_length=64, blank=True)
+   rfc = models.CharField(max_length=15, unique=True, null=True)
+   description = models.CharField(max_length=250, blank=True)
+   address
+   status
+
+   def __str__(self):
+      return
--- Growing_Enterprises/User/models.py
+++ Growing_Enterprises/User/models.py
@@ -1,3 +1,31 @@
+#Django
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+from Enterprise.models import EnterpriseProfile
+
+#Properties of the user
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
+    user_register_date = models.DateField(default=None, auto_now_add=True)
+    user_phone_number = models.DecimalField(max_digits=15, default=None, null=True)
+    foreign_lada = models.DecimalField(max_digits=3, default=None, null=True, blank=True)
+    date_of_birth = models.DateField(default=None,null=True)
+    user_rfc = models.CharField(max_length=18, unique=True, null=True)
+    user_email = models.EmailField(unique=True, null=False, blank=False)
+    user_full_name = models.CharField(max_length=250, null=False, blank=False)
+    user_address = models.CharField(max_length=500, null=True, blank=True)
+    user_country = models.CharField(max_length=40, null=True, blank=True)
+    user_gender = models.CharField(max_length=6, blank=True)
+    status = models.CharField(max=40, null=True, blank=True)
+    
+    def __str__(self):
+      return self.user, self.email
+    
+#Uploading all photos 
+class Image(models.Model):
+    #TODO falta agregar la direccion del archivo
+    image = models.FileField(upload_to='User')
+
+    
